b4e37b466d802f2ff3ebc33592dafcc8f89b8ada
notkaramel <hoangtuan11102@gmail.com>
2022-07-17 01:25:41 -0400

    Modify navbar to be more decent looking

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,21 @@
-// Main className is navbar
-
 import React from "react";
 import './scss/navbar.scss';
 
 class NavBar extends React.Component {
 
     render() {
-        
+
         return (
             <div className="navbar">
-                <a className="topright" href="#home">Antoine Phan</a>
-                <a className="topleft" href="#about">About Me</a>
-                <a className="topleft" href="#projects">Projects</a>
-                <a className="topleft" href="#skills">Skills</a>
-                <a className="topleft" href="#experience">Experience</a>
-                <a className="topleft" href="#citations">Citations</a>
+                <div className="topright">
+                    <a className="topright" href="#about-me">Antoine Phan</a>
+                </div>
+                <div className="topleft">
+                    <a href="#projects">Projects</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#experience">Experience</a>
+                    <a href="#citations">Citations</a>
+                </div>
             </div>
         );
     }
