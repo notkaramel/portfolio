dd338f941293e67ade00c064a30d1445d948e3c8
notkaramel <hoangtuan11102@gmail.com>
2022-07-18 14:49:53 -0400

    Visual Update for about-me, minor update for navbar

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -2,14 +2,14 @@ import React from "react";
 import './scss/about-me.scss'
 
 const social_media = () => {
-    const github_icon = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg";
-    const linkedin_icon = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg";
-    const email_icon = "https://upload.wikimedia.org/wikipedia/commons/5/50/TK_email_icon.svg";
+    const github_icon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg>
+    const linkedin_icon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" /></svg>
+    const email_icon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 417.1c-16.38 0-32.88-4.1-46.88-15.12L0 250.9v213.1C0 490.5 21.5 512 48 512h416c26.5 0 48-21.5 48-47.1V250.9l-209.1 151.1C288.9 412 272.4 417.1 256 417.1zM493.6 163C484.8 156 476.4 149.5 464 140.1v-44.12c0-26.5-21.5-48-48-48l-77.5 .0016c-3.125-2.25-5.875-4.25-9.125-6.5C312.6 29.13 279.3-.3732 256 .0018C232.8-.3732 199.4 29.13 182.6 41.5c-3.25 2.25-6 4.25-9.125 6.5L96 48c-26.5 0-48 21.5-48 48v44.12C35.63 149.5 27.25 156 18.38 163C6.75 172 0 186 0 200.8v10.62l96 69.37V96h320v184.7l96-69.37V200.8C512 186 505.3 172 493.6 163zM176 255.1h160c8.836 0 16-7.164 16-15.1c0-8.838-7.164-16-16-16h-160c-8.836 0-16 7.162-16 16C160 248.8 167.2 255.1 176 255.1zM176 191.1h160c8.836 0 16-7.164 16-16c0-8.838-7.164-15.1-16-15.1h-160c-8.836 0-16 7.162-16 15.1C160 184.8 167.2 191.1 176 191.1z" /></svg>
 
     const makeSocialMedia = (id, icon, url) => {
         return (<span id={id}>
             <a href={url} target="_blank" rel="noreferrer">
-                <img src={icon} alt={id + "_icon"} width="50px" />
+                {icon}
             </a>
         </span>);
     }
@@ -30,34 +30,24 @@ const social_media = () => {
 }
 
 const AboutMe = () => {
-    let greeting = <p>👋 Hi! I'm Antoine Phan.</p>;
-    let education = <p> 🎓 U1 Electrical Engineering @ McGill University </p>;
-    let location = <p> 📍 Montreal, Canada.</p>;
-    let cgpa = <p> 📈 CGPA: 3.52</p>;
-    let languages = <p> 🌐 Languages: English, French, Vietnamese, Spanish</p>;
+
     return (
         <div className="about-me" id="about-me">
-            <h1 className="section-title">~ About Me ~</h1>
-            <div className="about-me-box">
-                <div>
-                    {/* The profile picture spans 2 rows of the table */}
-                    <div>
-                        <section className="about-me-box-item">
-                            <img
-                                id="profile-picture"
-                                src="https://avatars.githubusercontent.com/u/24505220?v=4"
-                                alt="profile_pic">
-                            </img>
-                        </section>
-                        <section className="about-me-box-item" id="my-info">
-                            {greeting}
-                            {education}
-                            {location}
-                            {cgpa}
-                            {languages}
-                            {social_media()}
-                        </section>
-                    </div>
+            <h1 className="section-title" id="title">~ About Me ~</h1>
+            {/* Use a flexbox */}
+            <div className="about-me-flex">
+                <img
+                    className="about-me-flex-item"
+                    id="profile-picture"
+                    src="https://avatars.githubusercontent.com/u/24505220?v=4"
+                    alt="profile_pic">
+                </img>
+                <div className="about-me-flex-item" id="my-info">
+                    <p>👋 Hi! I'm Antoine Phan.</p>
+                    <p> 🎓 U1 Electrical Engineering @ McGill University </p>
+                    <p> 📍 Montreal, Canada.</p>
+                    <p> 📈 CGPA: 3.52</p>
+                    <p> 🌐 Languages: English, French, Vietnamese, Spanish</p>                    {social_media()}
                 </div>
             </div>
         </div>
