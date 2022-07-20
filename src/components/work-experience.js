a03601a126055dbbb7732613baea5b7b2240a27c
notkaramel <hoangtuan11102@gmail.com>
2022-07-19 21:16:02 -0400

    Visual Update

--- src/components/work-experience.js
+++ src/components/work-experience.js
@@ -1,13 +1,26 @@
 import React from "react";
+// First usage of style.sass with the new naming convention
+import "./scss/style.sass";
 
-class WorkExperience extends React.Component {
-    render() {
-        return (
-            <div className="past-exp" id="experience">
-                <h1 className="section-title">~Work Experience~</h1>
+export default function Experience() {
+    return (
+        <div className="section" id="experience">
+            <h1 className="section-title">Experience</h1>
+            <div className="container">
+                <div className="container-flex">
+                    <p className="container-flex-title">
+                        VICAS VIETNAM
+                    </p>
+                    <div className="container-flex-item">
+                        
+                    </div>
+                </div>
+                <div className="container-flex">
+                    <p className="container-flex-title">
+                        Hackathon Code.Jam(XI)
+                    </p>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default WorkExperience;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import NavBar from './components/navbar';
 import AboutMe from './components/about-me';
 import MySkills from './components/my-skills';
 import MyProjects from './components/my-projects';
-import WorkExperience from './components/work-experience';
+import Experience from './components/work-experience';
 import Footer from './components/footer';
 
 // Navigation bar section
@@ -24,7 +24,7 @@ content.render(
     <AboutMe />
     <MyProjects />
     <MySkills />
-    <WorkExperience />
+    <Experience />
   </React.StrictMode>
 );
 
